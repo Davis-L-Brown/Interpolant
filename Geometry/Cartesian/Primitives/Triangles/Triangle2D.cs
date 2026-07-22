3830c4804fe89e8baa17bafcc16e54ba4253efd9
Davis Brown <Davis.Brown@gentex.com>
2026-07-22 14:30:13 -0400

    updating triangles

--- Geometry/Cartesian/Primitives/Triangles/Triangle2D.cs
+++ Geometry/Cartesian/Primitives/Triangles/Triangle2D.cs
@@ -1,79 +1,12 @@
 ﻿using Geometry.Cartesian.Points;
 using Geometry.Cartesian.Primitives.LineSegments;
+using Geometry.Cartesian.Primitives.Triangles.Exceptions;
+using Geometry.Cartesian.Primitives.Triangles.Internal;
+using Geometry.Cartesian.Primitives.Vectors;
+using System;
 
 namespace Geometry.Cartesian.Primitives.Triangles
 {
-    /// <summary>
-    /// Defines a triangle in 2D cartesian space.
-    /// </summary>
-    public class Triangle2D : ITriangle<IPoint2D>
-    {
-        /// <inheritdoc/>
-        public IPoint2D V1 { get; }
-
-        /// <inheritdoc/>
-        public IPoint2D V2 { get; }
-
-        /// <inheritdoc/>
-        public IPoint2D V3 { get; }
-
-        /// <inheritdoc/>
-        public double Theta1 { get; }
-
-        /// <inheritdoc/>
-        public double Theta2 { get; }
-
-        /// <inheritdoc/>
-        public double Theta3 { get; }
-
-        /// <inheritdoc/>
-        public ILineSegment<IPoint2D> E1 { get; }
-
-        /// <inheritdoc/>
-        public ILineSegment<IPoint2D> E2 { get; }
-
-        /// <inheritdoc/>
-        public ILineSegment<IPoint2D> E3 { get; }
-
-        private IPoint2D Centroid { get; }
-        /// <inheritdoc/>
-        IPoint ITriangle<IPoint2D>.Centroid => Centroid;
-
-        /// <inheritdoc/>
-        public double Area { get; }
-
-
-        /// <summary>
-        /// Create a new instance of <see cref="Triangle2D"/>.
-        /// </summary>
-        public Triangle2D(IPoint2D v1, IPoint2D v2, IPoint2D v3)
-        {
-            // Assign the vertices
-            V1 = v1;
-            V2 = v2;
-            V3 = v3;
-
-            // Assign the edges
-            E1 = new LineSegment2D<IPoint2D>(V2, V3);
-            E2 = new LineSegment2D<IPoint2D>(V3, V1);
-            E3 = new LineSegment2D<IPoint2D>(V1, V2);
-
-            // Assgine the centroid 
-            var cx = (V1.X + V2.X + V3.X) / 3;
-            var cy = (V1.Y + V2.Y + V3.Y) / 3;
-            Centroid = new Point2D(cx, cy);
-
-            /************************************************************************
-             * Get the angles using law of cosines
-             * If you know the length of Edge A, B, and C, you can calculate θa via:
-             * θa = cos^-1((Eb^2 + Ec^2 - Ea^2) / (2 * Eb * Ec))
-             ************************************************************************/
-            Theta1 = TriangleMath.GetInteriorAngle(E2.Length, E3.Length, E1.Length);
-            Theta2 = TriangleMath.GetInteriorAngle(E3.Length, E1.Length, E2.Length);
-            Theta3 = TriangleMath.GetInteriorAngle(E1.Length, E2.Length, E3.Length);
-        }
-    }
-
     /// <summary>
     /// Defines a triangle in 2D cartesian space.
     /// </summary>
@@ -94,6 +27,7 @@ namespace Geometry.Cartesian.Primitives.Triangles
         /// <inheritdoc/>
         public TVertex V3 { get; }
 
+
         /// <inheritdoc/>
         public double Theta1 { get; }
 
@@ -103,18 +37,37 @@ namespace Geometry.Cartesian.Primitives.Triangles
         /// <inheritdoc/>
         public double Theta3 { get; }
 
-        /// <inheritdoc/>
-        public ILineSegment<TVertex> E1 { get; }
 
+        /// <summary>
+        /// Gets the edge opposite angle theta 1.
+        /// </summary>
+        public LineSegment2D<TVertex> E1 { get; }
         /// <inheritdoc/>
-        public ILineSegment<TVertex> E2 { get; }
+        ILineSegment<TVertex> ITriangle<TVertex>.E1 => E1;
 
+        /// <summary>
+        /// Gets the edge opposite angle theta 2.
+        /// </summary>
+        public LineSegment2D<TVertex> E2 { get; }
         /// <inheritdoc/>
-        public ILineSegment<TVertex> E3 { get; }
+        ILineSegment<TVertex> ITriangle<TVertex>.E2 => E2;
+
+        /// <summary>
+        /// Gets the edge opposite angle theta 3.
+        /// </summary>
+        public LineSegment2D<TVertex> E3 { get; }
+        /// <inheritdoc/>
+        ILineSegment<TVertex> ITriangle<TVertex>.E3 => E3;
+
 
         private IPoint2D Centroid { get; }
         /// <inheritdoc/>
         IPoint ITriangle<TVertex>.Centroid => Centroid;
+
+        /// <summary>
+        /// The signed area of the triangle.
+        /// </summary>
+        public double SignedArea { get; }
 
         /// <inheritdoc/>
         public double Area { get; }
@@ -123,19 +76,45 @@ namespace Geometry.Cartesian.Primitives.Triangles
         /// <summary>
         /// Create a new instance of <see cref="Triangle2D{TVertex}"/>.
         /// </summary>
-        public Triangle2D(TVertex v1, TVertex v2, TVertex v3)
+        public Triangle2D(
+            TVertex v1, TVertex v2, TVertex v3,
+            double relativeDegeneracyTolerance = TriangleValidator.DefaultDegeneracyTolerance)
         {
             // Assign the vertices
-            V1 = v1;
-            V2 = v2;
-            V3 = v3;
+            V1 = v1 == null ? throw new ArgumentNullException(nameof(v1)) : v1;
+            V2 = v2 == null ? throw new ArgumentNullException(nameof(v2)) : v2;
+            V3 = v3 == null ? throw new ArgumentNullException(nameof(v3)) : v3;
+
+            // Check for degeneracy using doubled area
+            Vector2D v12 = Vector2D.FromPoints(V1, V2);
+            Vector2D v13 = Vector2D.FromPoints(V1, V3);
+            Vector2D v23 = Vector2D.FromPoints(V2, V3);
+
+            // use V12 & V13 to calculate cross product
+            // because they have the same origin
+            double signedDoubleArea = Vector2D.Cross(v12, v13);
+            double doubledArea = Math.Abs(signedDoubleArea);
+
+            if (!TriangleValidator.IsDegenerate(
+                doubledArea,
+                v12.MagnitudeSquared,
+                v13.MagnitudeSquared,
+                v23.MagnitudeSquared,
+                relativeDegeneracyTolerance))
+                throw new DegenerateTriangleException();
+
+            // Calculate and assinge signed area
+            SignedArea = 0.5 * signedDoubleArea;
+
+            // Assign area
+            Area = Math.Abs(SignedArea);
 
             // Assign the edges
             E1 = new LineSegment2D<TVertex>(V2, V3);
             E2 = new LineSegment2D<TVertex>(V3, V1);
             E3 = new LineSegment2D<TVertex>(V1, V2);
 
-            // Assgine the centroid 
+            // Assgin the centroid 
             var cx = (V1.X + V2.X + V3.X) / 3;
             var cy = (V1.Y + V2.Y + V3.Y) / 3;
             Centroid = new Point2D(cx, cy);
@@ -149,5 +128,20 @@ namespace Geometry.Cartesian.Primitives.Triangles
             Theta2 = TriangleMath.GetInteriorAngle(E3.Length, E1.Length, E2.Length);
             Theta3 = TriangleMath.GetInteriorAngle(E1.Length, E2.Length, E3.Length);
         }
+    }
+
+
+    /// <summary>
+    /// Defines a triangle in 2D cartesian space.
+    /// </summary>
+    public class Triangle2D : Triangle2D<IPoint2D>
+    {
+        /// <summary>
+        /// Create a new instance of <see cref="Triangle2D"/>.
+        /// </summary>
+        public Triangle2D(
+            IPoint2D v1, IPoint2D v2, IPoint2D v3,
+            double relativeDegeneracyTolerance = TriangleValidator.DefaultDegeneracyTolerance)
+            : base(v1, v2, v3, relativeDegeneracyTolerance) { }
     }
 }
--- Geometry/Cartesian/Primitives/Triangles/Triangle3D.cs
+++ Geometry/Cartesian/Primitives/Triangles/Triangle3D.cs
@@ -1,80 +1,12 @@
 ﻿using Geometry.Cartesian.Points;
 using Geometry.Cartesian.Primitives.LineSegments;
+using Geometry.Cartesian.Primitives.Triangles.Exceptions;
+using Geometry.Cartesian.Primitives.Triangles.Internal;
+using Geometry.Cartesian.Primitives.Vectors;
+using System;
 
 namespace Geometry.Cartesian.Primitives.Triangles
 {
-    /// <summary>
-    /// Defines a triangle in 3D cartesian space.
-    /// </summary>
-    public class Triangle3D : ITriangle<IPoint3D>
-    {
-        /// <inheritdoc/>
-        public IPoint3D V1 { get; }
-
-        /// <inheritdoc/>
-        public IPoint3D V2 { get; }
-
-        /// <inheritdoc/>
-        public IPoint3D V3 { get; }
-
-        /// <inheritdoc/>
-        public double Theta1 { get; }
-
-        /// <inheritdoc/>
-        public double Theta2 { get; }
-
-        /// <inheritdoc/>
-        public double Theta3 { get; }
-
-        /// <inheritdoc/>
-        public ILineSegment<IPoint3D> E1 { get; }
-
-        /// <inheritdoc/>
-        public ILineSegment<IPoint3D> E2 { get; }
-
-        /// <inheritdoc/>
-        public ILineSegment<IPoint3D> E3 { get; }
-
-        private IPoint3D Centroid { get; }
-        /// <inheritdoc/>
-        IPoint ITriangle<IPoint3D>.Centroid => Centroid;
-
-        /// <inheritdoc/>
-        public double Area { get; }
-
-
-        /// <summary>
-        /// Create a new instance of <see cref="Triangle3D"/>.
-        /// </summary>
-        public Triangle3D(IPoint3D v1, IPoint3D v2, IPoint3D v3)
-        {
-            // Assign the vertices
-            V1 = v1;
-            V2 = v2;
-            V3 = v3;
-
-            // Assign the edges
-            E1 = new LineSegment3D<IPoint3D>(V2, V3);
-            E2 = new LineSegment3D<IPoint3D>(V3, V1);
-            E3 = new LineSegment3D<IPoint3D>(V1, V2);
-
-            // Assgine the centroid 
-            var cx = (V1.X + V2.X + V3.X) / 3;
-            var cy = (V1.Y + V2.Y + V3.Y) / 3;
-            var cz = (V1.Z + V2.Z + V3.Z) / 3;
-            Centroid = new Point3D(cx, cy, cz);
-
-            /************************************************************************
-             * Get the angles using law of cosines
-             * If you know the length of Edge A, B, and C, you can calculate θa via:
-             * θa = cos^-1((Eb^2 + Ec^2 - Ea^2) / (2 * Eb * Ec))
-             ************************************************************************/
-            Theta1 = TriangleMath.GetInteriorAngle(E2.Length, E3.Length, E1.Length);
-            Theta2 = TriangleMath.GetInteriorAngle(E3.Length, E1.Length, E2.Length);
-            Theta3 = TriangleMath.GetInteriorAngle(E1.Length, E2.Length, E3.Length);
-        }
-    }
-
     /// <summary>
     /// Defines a triangle in 3D cartesian space.
     /// </summary>
@@ -95,6 +27,7 @@ namespace Geometry.Cartesian.Primitives.Triangles
         /// <inheritdoc/>
         public TVertex V3 { get; }
 
+
         /// <inheritdoc/>
         public double Theta1 { get; }
 
@@ -104,14 +37,28 @@ namespace Geometry.Cartesian.Primitives.Triangles
         /// <inheritdoc/>
         public double Theta3 { get; }
 
-        /// <inheritdoc/>
-        public ILineSegment<TVertex> E1 { get; }
 
+        /// <summary>
+        /// Gets the edge opposite angle theta 1.
+        /// </summary>
+        public LineSegment3D<TVertex> E1 { get; }
         /// <inheritdoc/>
-        public ILineSegment<TVertex> E2 { get; }
+        ILineSegment<TVertex> ITriangle<TVertex>.E1 => E1;
 
+        /// <summary>
+        /// Gets the edge opposite angle theta 2.
+        /// </summary>
+        public LineSegment3D<TVertex> E2 { get; }
         /// <inheritdoc/>
-        public ILineSegment<TVertex> E3 { get; }
+        ILineSegment<TVertex> ITriangle<TVertex>.E2 => E2;
+
+        /// <summary>
+        /// Gets the edge opposite angle theta 3.
+        /// </summary>
+        public LineSegment3D<TVertex> E3 { get; }
+        /// <inheritdoc/>
+        ILineSegment<TVertex> ITriangle<TVertex>.E3 => E3;
+
 
         private IPoint3D Centroid { get; }
         /// <inheritdoc/>
@@ -124,12 +71,37 @@ namespace Geometry.Cartesian.Primitives.Triangles
         /// <summary>
         /// Create a new instance of <see cref="Triangle3D{TVertex}"/>.
         /// </summary>
-        public Triangle3D(TVertex v1, TVertex v2, TVertex v3)
+        public Triangle3D(
+            TVertex v1, TVertex v2, TVertex v3,
+            double relativeDegeneracyTolerance = TriangleValidator.DefaultDegeneracyTolerance)
         {
             // Assign the vertices
-            V1 = v1;
-            V2 = v2;
-            V3 = v3;
+            V1 = v1 == null ? throw new ArgumentNullException(nameof(v1)) : v1;
+            V2 = v2 == null ? throw new ArgumentNullException(nameof(v2)) : v2;
+            V3 = v3 == null ? throw new ArgumentNullException(nameof(v3)) : v3;
+
+            // Check for degeneracy using doubled area
+            Vector3D v12 = Vector3D.FromPoints(V1, V2);
+            Vector3D v13 = Vector3D.FromPoints(V1, V3);
+            Vector3D v23 = Vector3D.FromPoints(V2, V3);
+
+            // the magnitude of V12 x V13 is the area of the parallelogram
+            // spanned by the vectors
+            Vector3D doubleAreaVector = Vector3D.Cross(v12, v13);
+            double doubledArea = doubleAreaVector.Magnitude;
+
+            if (!TriangleValidator.IsDegenerate(
+                doubledArea,
+                v12.MagnitudeSquared,
+                v12.MagnitudeSquared,
+                v23.MagnitudeSquared,
+                relativeDegeneracyTolerance))
+                throw new DegenerateTriangleException();
+
+
+
+            // Calculate and assign area
+            Area = doubledArea * 0.5;
 
             // Assign the edges
             E1 = new LineSegment3D<TVertex>(V2, V3);
@@ -151,5 +123,21 @@ namespace Geometry.Cartesian.Primitives.Triangles
             Theta2 = TriangleMath.GetInteriorAngle(E3.Length, E1.Length, E2.Length);
             Theta3 = TriangleMath.GetInteriorAngle(E1.Length, E2.Length, E3.Length);
         }
+    }
+
+
+
+    /// <summary>
+    /// Defines a triangle in 3D cartesian space.
+    /// </summary>
+    public class Triangle3D : Triangle3D<IPoint3D>
+    {
+        /// <summary>
+        /// Create a new instance of <see cref="Triangle3D"/>.
+        /// </summary>
+        public Triangle3D(
+            IPoint3D v1, IPoint3D v2, IPoint3D v3,
+            double relativeDegeneracyTolerance = TriangleValidator.DefaultDegeneracyTolerance)
+            : base (v1, v2, v3, relativeDegeneracyTolerance) { }
     }
 }
